6ceb6fa1006e3e633f8bf867043c3a2961d7d88b
CeIIardoor <laouinayassine@gmail.com>
2023-04-22 22:58:30 +0000

    Authorization Prototype

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {HomeService} from "./home.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent implements OnInit {
+  public helloMessage: string = "";
+  constructor(
+    private httpClient: HttpClient,
+    private homeService: HomeService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.homeService.getHelloMessage().subscribe(
+      response => {
+        this.helloMessage = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,7 +2,7 @@ import {Routes} from "@angular/router";
 import {IndexComponent} from "./index/index.component";
 import {AdminComponent} from "./admin/admin.component";
 import {AuthGuard} from "./_auth/auth.guard";
-import {UserComponent} from "./user/user.component";
+import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {ForbiddenComponent} from "./forbidden/forbidden.component";
 import {RegisterComponent} from "./register/register.component";
@@ -10,7 +10,7 @@ import {RegisterComponent} from "./register/register.component";
 export const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: {roles: ["ROLE_ADMIN"]}},
-  {path: 'user', component: UserComponent, canActivate: [AuthGuard], data: {roles: ["ROLE_ADMIN", "ROLE_UTILISATEUR"]}},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard], data: {roles: ["ROLE_ADMIN", "ROLE_UTILISATEUR"]}},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'forbidden', component: ForbiddenComponent},

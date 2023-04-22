11fc999a3402841cd3c6f9785a72afff1a4d0b0f
CeIIardoor <laouinayassine@gmail.com>
2023-04-22 21:57:13 +0000

    Adding Register

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,11 +5,13 @@ import {AuthGuard} from "./_auth/auth.guard";
 import {UserComponent} from "./user/user.component";
 import {LoginComponent} from "./login/login.component";
 import {ForbiddenComponent} from "./forbidden/forbidden.component";
+import {RegisterComponent} from "./register/register.component";
 
 export const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: {roles: ["ROLE_ADMIN"]}},
   {path: 'user', component: UserComponent, canActivate: [AuthGuard], data: {roles: ["ROLE_ADMIN", "ROLE_UTILISATEUR"]}},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'forbidden', component: ForbiddenComponent},
 ];

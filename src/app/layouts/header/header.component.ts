d0314ab188835ac48bea9d5f62f85204a0900027
CeIIardoor <laouinayassine@gmail.com>
2023-05-02 11:48:41 +0100

    restructurings

---
+++ src/app/layouts/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../_auth/auth.service";
+import { Router } from "@angular/router";
+import {routeNames} from "../../routes";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+
+})
+export class HeaderComponent implements OnInit {
+  constructor(
+    private userAuthService: AuthService,
+    private router: Router,
+    ) {}
+
+  ngOnInit(): void {}
+
+  public isAuthenticated() : boolean {
+    return this.userAuthService.isAuthenticated();
+  }
+  logout() {
+    this.router.navigate(['/'+ routeNames.index]).then(() => console.log("logout to index : "));
+    this.userAuthService.clear();
+  }
+
+    protected readonly routeNames = routeNames;
+}
